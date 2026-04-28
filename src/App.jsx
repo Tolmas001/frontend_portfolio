3f5a6a39626c385d6236dd1a199438046c1593a7
Tolmas001 <urinovtolmas20@gmail.com>
2026-04-29 03:26:13 +0500

    feat: implement LanguageProvider and integrate multi-language support into Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { LanguageProvider } from './LanguageContext';
 import { ThemeProvider } from './ThemeContext';
 import { useScrollRevealAll } from './hooks/useScrollReveal';
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { useLang } from '../LanguageContext';
 import { useTheme } from '../ThemeContext';
 
 const Navbar = () => {
